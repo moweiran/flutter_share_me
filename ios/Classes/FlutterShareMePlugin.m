543cede8898e3960e48729bb3edc89bbe1ec5b7f
lzy <iamlzy@163.com>
2022-04-19 18:18:34 +0800

    fix bug

--- ios/Classes/FlutterShareMePlugin.m
+++ ios/Classes/FlutterShareMePlugin.m
@@ -1,20 +1,15 @@
 #import "FlutterShareMePlugin.h"
+#if __has_include(<flutter_share_me/flutter_share_me-Swift.h>)
+#import <flutter_share_me/flutter_share_me-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "flutter_share_me-Swift.h"
+#endif
 
 @implementation FlutterShareMePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_share_me"
-            binaryMessenger:[registrar messenger]];
-  FlutterShareMePlugin* instance = [[FlutterShareMePlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    [SwiftFlutterShareMePlugin registerWithRegistrar:registrar];
 }
-
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
-
 @end
